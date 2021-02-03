86ec3c008d022654c2a2749df449056196466374
vinizeus <viniciusonezio35@gmail.com>
2021-02-03 18:58:42 -0400

    First commit

---
+++ database/menu/help.js
@@ -0,0 +1,49 @@
+const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+        return `
+╔══✪〘 Informações 〙✪══
+║
+║───────⊹⊱✫⊰⊹───────
+║➩ ❍ wa.me/556993899391
+║➩ ❍ Prefix: 「  ${prefix}  」
+║➩ ❍ Criador: ${botName}
+║➩ ❍ Nome: ${pushname}️
+║➩ ❍ XP: ${reqXp}
+║➩ ❍ Money: ${uangku}
+───────⊹⊱✫⊰⊹───────
+
+
+                     𝚉𝙴𝚄𝚂
+
+───────⊹⊱✫⊰⊹───────
+║➩ ❍ *${prefix}info*
+║➩ ❍ *${prefix}blocklist*
+║➩ ❍ *${prefix}chatlist*
+║➩ ❍ *${prefix}ping*
+║➩ ❍ *${prefix}bugreport*
+║➩ ❍ *${prefix}logomaker*
+║➩ ❍ *${prefix}imagemaker*
+║➩ ❍ *${prefix}stickermaker*
+║➩ ❍ *${prefix}search*
+║➩ ❍ *${prefix}education*
+║➩ ❍ *${prefix}kerang*
+║➩ ❍ *${prefix}downloader*
+║➩ ❍ *${prefix}meme*
+║➩ ❍ *${prefix}group*
+║➩ ❍ *${prefix}sound*
+║➩ ❍ *${prefix}music*
+║➩ ❍ *${prefix}islam*
+║➩ ❍ *${prefix}stalk*
+║➩ ❍ *${prefix}wibu*
+║➩ ❍ *${prefix}18+*
+║➩ ❍ *${prefix}fun*
+║➩ ❍ *${prefix}tod*
+║➩ ❍ *${prefix}information*
+║➩ ❍ *${prefix}stayonscreen*
+║➩ ❍ *${prefix}xp*
+║➩ ❍ *${prefix}limit*
+║➩ ❍ *${prefix}owner*
+║➩ ❍ *${prefix}other*
+║➩ Aumente seu level interagindo no grupo!!
+───────⊹⊱✫⊰⊹───────`
+}
+exports.help = help
